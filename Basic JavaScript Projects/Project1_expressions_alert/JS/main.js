34ca83ae29e645120d6c1515f735c3f30ecb2ec2
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-07 19:55:09 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+
+
+var sent1 = "\"This is the beginning of the string\""; //variables assigne the value//
+
+var sent2 = "\"and this is the end of string \""; //variables assigne the value//
+
+
+window.alert(sent1);  //how to use window alert//
+
+document.write(sent2); //how to write the assigned to a variable on an html page //
+
+document.write("\"This is the beginning of the string," + "and this is the end of string \"");  //how to concentate the text//
+
+
+12+12  // expression//
+
+document.write(12+12);   //how to write an expression//
+
+
+
+
+
---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main_1.js
@@ -0,0 +1,26 @@
+document.write('Hello,World!'); // alertbox for the users//
+
+var A ="My name is Arvind Kumar"; //how to assign a value to a variable//
+window.alert(A);  //How to display that value on a HTML Page using java script//
+
+
+document.write("Arvind told pinku, \"Do your breakfast, or I will tell mom !\"<br>\"Do your homework and enjoy your evening !\" Mom responded ");
+
+//how to add inverted commas in between text //
+document.write("\"Be who you are and say what you feel,"  //concenatted text  //
++"because those who mind don\'t matter and those who matter don\'t mind"+"-Dr Arvg ");
+
+
+var b = "concentated "+"string"
+
+document.write(b)
+
+
+var family = "The Khokhar\'s",Dad ="Randhir singh" ,Mom = "Kesi",Brother ="Ashwani", Wife = "pinku",Daughter ="anshika";
+
+document.write(Daughter);
+
+
+4+4
+
+document.write(4+4);
