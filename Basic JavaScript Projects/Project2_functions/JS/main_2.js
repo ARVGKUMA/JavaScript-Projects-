f8675e5117baa776baf7ba949d2db378bfa0fcc2
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-08 16:09:48 +0000

    Update main_2.js

--- Basic JavaScript Projects/Project2_functions/JS/main_2.js
+++ Basic JavaScript Projects/Project2_functions/JS/main_2.js
@@ -2,3 +2,11 @@ function My_function (){
     var str ="This is the Happy text!";
     document.getElementById(Happy_text).innerHTML= str;
 }
+
+
+
+function myFunction (){
+    var sentence ="This is the Happy text!";
+    sentence += "and I am happy about it ";
+    document.getElementById("cocatenate").innerHTML= sentence;
+}
