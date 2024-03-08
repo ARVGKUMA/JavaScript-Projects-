9257f61774db9f0e1e9e0e9a165fd0c28ed8e1e7
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-08 17:33:37 +0000

    Update main_4.js

--- Basic JavaScript Projects/Project3_math_operators /JS/main_4.js
+++ Basic JavaScript Projects/Project3_math_operators /JS/main_4.js
@@ -1,1 +1,4 @@
-
+function addition_function(){
+    var addition = 100+200;
+    document.getElementById("math").innerHTML= "100+200 ="+addition; // function is used to do mathematics calculation using operator//
+}
