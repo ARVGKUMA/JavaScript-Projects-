fd0cc82e337d70022ec6f4751ad45790eb108a83
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-08 17:56:08 +0000

    Update main_4.js

--- Basic JavaScript Projects/Project3_math_operators /JS/main_4.js
+++ Basic JavaScript Projects/Project3_math_operators /JS/main_4.js
@@ -2,3 +2,18 @@ function addition_function(){
     var addition = 100+200;
     document.getElementById("math").innerHTML= "100+200 ="+addition; // function is used to do mathematics calculation using operator//
 }
+
+function subtraction_function(){
+    var subtraction = 200-100;
+    document.getElementById("Math").innerHTML= "200-100 ="+subtraction; // function is used to do mathematics calculation using operator//
+}
+
+function multiplication_function(){
+    var multiplication = 200*100;
+    document.getElementById("Math_").innerHTML= "200*100 ="+multiplication; // function is used to do mathematics calculation using operator//
+}
+
+function division_function(){
+    var division = 200/100;
+    document.getElementById("Math_1").innerHTML= "200/100 ="+division; // function is used to do mathematics calculation using operator//
+}
