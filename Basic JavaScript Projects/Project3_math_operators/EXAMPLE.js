2e0b0b1763dc22ba2bdceb85fbc7700da78c863d
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-10 10:35:39 +0000

    Update EXAMPLE.js

--- Basic JavaScript Projects/Project3_math_operators/EXAMPLE.js
+++ Basic JavaScript Projects/Project3_math_operators/EXAMPLE.js
@@ -9,7 +9,7 @@ function my_dictionary(){
     document.getElementById("Dictionary").innerHTML = Animal.Breed;
 };
 
-document.write(10>5);
+document.write(10>5); //
 
 document.write(10<4);
 
@@ -47,15 +47,27 @@ A = "Happy"
 
 B = "PINKU"
 
-document.write(A===B);
+document.write(A===B); //equal to operator//
 
-document.write(10>2  &&  10>4);
+document.write(10>2  &&  10>4);  // and operator//
 
-document.write(10>12  &&  10>14);
 
-document.write(10>12  ||  10>4);
+document.write(10>12  &&  10>14);  // and operator//
 
-document.write(10>15  &&  10>20);
+
+document.write(10>12  ||  10>4); //OR operator//
+
+
+document.write(10>15  &&  10>20); // and operator//
+
+
+function not_function() {
+    document.getElementById("Not").innerHTML =!(20>10); //Not Operator//
+}
+
+
+function not_function() {
+    document.getElementById("Not").innerHTML =!(5>10); //Not Operator//
 
 
 
