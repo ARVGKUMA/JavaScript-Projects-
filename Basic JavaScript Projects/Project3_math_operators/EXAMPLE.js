903352489c2271fd635e5ffa29a21359da94390b
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-10 07:50:40 +0000

    Add files via upload

---
+++ Basic JavaScript Projects/Project3_math_operators/EXAMPLE.js
@@ -0,0 +1,21 @@
+function my_dictionary(){
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Alsesation",
+        Age:12,
+        Sound:"Bark"
+    } ;
+    document.getElementById("Dictionary").innerHTML = Animal.Breed;
+};
+
+document.write(10>5);
+
+document.write(10<4);
+
+
+
+
+
+
+
