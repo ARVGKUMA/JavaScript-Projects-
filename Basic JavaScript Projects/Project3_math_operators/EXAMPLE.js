bc1d3caba9c3de1700334a57d08322613707e72f
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-10 10:18:55 +0000

    Update EXAMPLE.js

--- Basic JavaScript Projects/Project3_math_operators/EXAMPLE.js
+++ Basic JavaScript Projects/Project3_math_operators/EXAMPLE.js
@@ -13,6 +13,50 @@ document.write(10>5);
 
 document.write(10<4);
 
+console.log(100+100);
+
+
+console.log(100>200);
+
+
+document.write(10==10);
+
+
+document.write(3==10);
+
+
+x = 10;
+y = 10;
+document.write(x===y);
+
+
+
+x = 20;
+y = 10;
+document.write(x===y);
+
+
+A = "Happy"
+
+B = "Happy"
+
+document.write(A===B);
+
+
+A = "Happy"
+
+B = "PINKU"
+
+document.write(A===B);
+
+document.write(10>2  &&  10>4);
+
+document.write(10>12  &&  10>14);
+
+document.write(10>12  ||  10>4);
+
+document.write(10>15  &&  10>20);
+
 
 
 
