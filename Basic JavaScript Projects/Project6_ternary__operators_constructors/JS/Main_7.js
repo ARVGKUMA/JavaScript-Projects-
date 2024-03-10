d2b7cc3f8aec7f00c41fcf25698b07747bbf06dd
ARVGKUMA <152650570+ARVGKUMA@users.noreply.github.com>
2024-03-10 10:58:14 +0000

    Update Main_7.js

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/Main_7.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/Main_7.js
@@ -1,1 +1,78 @@
+function my_dictionary(){
+    var Animal = {
+        Species:"Dog",
+        Color:"Black",
+        Breed:"Alsesation",
+        Age:12,
+        Sound:"Bark"
+    } ;
+    document.getElementById("Dictionary").innerHTML = Animal.Breed;
+};
 
+document.write(10>5); //
+
+document.write(10<4);
+
+console.log(100+100);
+
+
+console.log(100>200);
+
+
+document.write(10==10);
+
+
+document.write(3==10);
+
+
+x = 10;
+y = 10;
+document.write(x===y);
+
+
+
+x = 20;
+y = 10;
+document.write(x===y);
+
+
+A = "Happy"
+
+B = "Happy"
+
+document.write(A===B);
+
+
+A = "Happy"
+
+B = "PINKU"
+
+document.write(A===B); //equal to operator//
+
+document.write(10>2  &&  10>4);  // and operator//
+
+
+document.write(10>12  &&  10>14);  // and operator//
+
+
+document.write(10>12  ||  10>4); //OR operator//
+
+
+document.write(10>15  &&  10>20); // and operator//
+
+
+function not_function() {
+    document.getElementById("Not").innerHTML =!(20>10); //Not Operator//
+}
+
+
+function not_function() {
+    document.getElementById("Not").innerHTML =!(5>10); //Not Operator//
+}
+
+function Ride_function(){
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride =(Height<52)? "You are too short" :"You are tall enough"
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride. ";
+}
